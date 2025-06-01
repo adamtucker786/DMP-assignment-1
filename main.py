b2e6c882dc38b4523b896c0a916472a8c937bde7
adamtucker786 <adam.tucker.786@cranfield.ac.uk>
2025-06-01 20:35:13 +0100

    button class created

--- main.py
+++ main.py
@@ -1,24 +1,59 @@
 import pygame
 pygame.init()
 
-# create constants for screen setup
+
+# create constants for Screen setup
 SCREENWIDTH = 1000
 SCREENHEIGHT = 500
 SCREENCAPTION = "asssignment 1 DMP"
 SCREENICON = pygame.image.load("assets\RandomLogo.jpg")
 
+# colours
 WHITE = 255, 255, 255
 
+# fonts
+BUTTON_FONT = pygame.font.SysFont("comicsansms", 40)
+
 #creating display with a caption and icon using function from pygame libary
-screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+Screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 pygame.display.set_caption(SCREENCAPTION)
 pygame.display.set_icon(SCREENICON)
 
 
+EMPTY_BUTTON_IMAGE = pygame.transform.scale(pygame.image.load("assets\empty button.png"), (400, 75))
+# class for buttons which takes inputs for the images and text to display and displays the text in that Image
+# also takes coordinate position to display the buttons at
+class Buttons:
+  def __init__(self, Image, text, x_pos, y_pos):
+    self.costume_index = 0
+    self.Image = Image
+    self.x_pos = x_pos
+    self.y_pos = y_pos
+    # command will create a set of coordinates that correspond to the size and location of the Image and text
+    self.rect = self.Image.get_rect(center=(self.x_pos, self.y_pos))
+    self.text = text
+    self.button_text = BUTTON_FONT.render(self.text, True, (0,0,0))
+    self.text_rect = self.button_text.get_rect(center=(self.x_pos, self.y_pos))
+
+  # this method of the class will display the object to the Screen at the given Screen position
+  def draw_button(self):
+    Screen.blit(self.Image, self.rect)
+    Screen.blit(self.button_text, self.text_rect)
+
+
+
+# function to display text on the Screen
+def AddText(text, font, position, size, colour):
+  Font = pygame.font.font(None, size)
+  AddedText = Font.render(text, True, colour)
+  TextRect = AddedText
+  pass
+
 
 # function to activate the menu
 def DisplayMenu():
-  screen.fill(WHITE)
+  Screen.fill(WHITE)
+
 
 global Current_Page 
 Current_Page = "Menu"
