8fecc1a6b4e4df6aedb1dcda223e75491362c391
adamtucker786 <adam.tucker.786@cranfield.ac.uk>
2025-06-09 01:19:07 +0100

    full commenting and limits on the custom timer meaning max 59 seconds and no negative values

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#importing and initialising relevant libaries
 import pygame
 pygame.init()
 
@@ -17,62 +18,92 @@ BUTTON_FONT = pygame.font.SysFont("comicsansms", 40)
 
 #creating display with a caption and icon using function from pygame libary
 Screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+# applying the screen setup already set variables
 pygame.display.set_caption(SCREENCAPTION)
 pygame.display.set_icon(SCREENICON)
 
+
+# initialising the pygame clock to control the speed the system runs which allows for acurate timer speed
 FPS = 60
 clock = pygame.time.Clock()
 
 
+# class for the timer system 
 class Timer:
+  # initialiing an instance of the class requires the start length in mins and seconds
   def __init__(self, StartMinutes, StartSeconds):
     self.Minutes = StartMinutes
     self.Seconds = StartSeconds
     self.StartMinutes = StartMinutes
     self.StartSeconds = StartSeconds
+    # making use of a speed variable meaning i can speed up the timer if wanted
     self.Speed = 1
 
+  # reset method causes timer to return to its start length
   def Reset(self):
     self.Minutes = self.StartMinutes
     self.Seconds = self.StartSeconds
 
+  # method for counting down through the timer
   def decrement(self):
+    # counting down by one fraction of the timer speed every frame so the clock is at the right speed
     self.Seconds -= self.Speed/FPS
+    # decreasing the minutes when the seconds reach zero so that the minute goes down and the second goes back to 59
     if self.Seconds < 0:
       self.Seconds = 59
       self.Minutes -= 1
 
+  # method to display the timer to the middle of the screen using my add text method
   def display(self):
+    # casting the values to an int so only whole nmbers are displayed on the screen as in reality the timer counts to fraction of a second accuracy
     AddText(f"{int(self.Minutes)}:{int(self.Seconds)}", (400, 200), 80, BLACK)
 
+  # method to pause the timer by setting the speed to zero as this means each frame zero is taken from the seconds so timer stays same
   def pause(self):
     self.Speed = 0
   
+  # methpd to retun speed to 1 so that the timer returns to counting down by one every second
   def resume(self):
     self.Speed = 1
-
-  def isFinished(self):
-    if self.Minutes < 0:
-      return True
+  
+  # checking if the timer is finished and returning true is it has and false if it isnt finished
+  def isFinished(self)-> bool:
+    finished = False
+    # if the minutes and seconds are both less than zero then timer is finished so set value to return as True
+    if (self.Minutes < 0):
+      finished = True
     else:
-      return False
+      finished = False
+    return finished
 
 
+# creating a class for custim timers holding methods not used by the base pomodoro timer
+# class inherits from the timer class which acts as a super class so that customTimer inherits its methods and attributes
 class customTimer(Timer):
   def __init__(self, StartMinutes, StartSeconds):
     super().__init__(StartMinutes, StartSeconds)
 
+  # functions used in creation of the custom timer to set its length
   def increaseMinutes(self):
     self.Minutes += 1
-
+  
   def decreaseMinutes(self):
     self.Minutes -= 1
+    # checking if the user has set the minutes count bellow zero and restoring it to zero to make impossible to have -ve time
+    if self.Minutes < 0:
+      self.Minutes = 0
 
   def increaseSeconds(self):
     self.Seconds += 1
+    # creating a max of 59 in the seconds section and making the counter return to zero if the user goes over for faster time selection
+    if self.Seconds >= 60:
+      self.Seconds = 0
 
   def decreaseSeconds(self):
     self.Seconds -= 1
+    # setting the seconds to 59 if the user tries to go bellow zero seconds to remove error of -ve time and for faster time selection
+    if self.Seconds < 0:
+      self.Seconds = 59
 
   
 
@@ -97,12 +128,17 @@ class Buttons:
     Screen.blit(self.Image, self.rect)
     Screen.blit(self.button_text, self.text_rect)
 
+  # method checking is the button has been clicked
   def isPressed(self)-> bool:
+    # creating a boolean variable to return
     pressed = False
+    # using pygame collidepoint method to detect if passed in point is in collision with the objects rect
+    # passing in the position of the event which is the position of the mouse click in this instance
     if self.rect.collidepoint(event.pos):
       pressed = True
     return pressed
-  
+
+
 # function to display text on the Screen
 def AddText(text, position, size, colour):
   Font = pygame.font.Font(None, size)
@@ -110,39 +146,56 @@ def AddText(text, position, size, colour):
   Screen.blit(AddedText, position)
 
 
+# cresting button objects for navigation to the desired timer section
 POMODORO_BUTTON = Buttons(EMPTY_BUTTON_IMAGE, "Pomodoro Timer", 220, 200)
 CUSTOM_BUTTON = Buttons(EMPTY_BUTTON_IMAGE, "Custom Timer", 220, 300)
 # function to activate the menu
 def MenuScreen():
+  # blanking out the screen and creating a white background each frame to create animation
   Screen.fill(WHITE)
+  #calling the draw methonds of the buttons so they display on the screen
   POMODORO_BUTTON.draw_button()
   CUSTOM_BUTTON.draw_button()
+  # adding the title to the page for cosmetics with my add text function
   AddText("study timer assistant", (400, 50), 60, BLACK)
 
 
+# creating objects of the buttons class for pausing and resuming the timer
 PAUSE_BUTTON = Buttons(EMPTY_BUTTON_IMAGE, "Pause Timer", 200, 400)
 RESUME_BUTTON = Buttons(EMPTY_BUTTON_IMAGE, "Resume Timer", 600, 400)
+# creating an instance of the timer class for the main pomodoro timer with initial values of 25 minutes and zero seconds
 PomdoroTimer = Timer(0, 10)
 def TimerScreen():
   Screen.fill(WHITE)
+  # title for the page
   AddText("Study time", (300, 50), 60, BLACK)
+  # displaying the timer with the class method
   PomdoroTimer.display()
+  # displaying the pause and resume buttons with the class method
   PAUSE_BUTTON.draw_button()
   RESUME_BUTTON.draw_button()
 
 
+# loading arrow images for use in buttons in timer creation stage
 INCREASE_ARROW_iMAGE = pygame.transform.scale(pygame.image.load("assets\_UpArrowImage.png"), (50, 50))
 DECREASE_ARROW_iMAGE = pygame.transform.scale(pygame.image.load("assets\_DownArrowImage.png"), (50, 50))
+# cresting increase and decrease buttons for minutes and seconds of the custom timer
 MINUTE_INCREASE_BUTTON = Buttons(INCREASE_ARROW_iMAGE, "", 50, 200)
 SECOND_INCREASE_BUTTON = Buttons(INCREASE_ARROW_iMAGE, "", 950, 200)
 MINUTE_DECREASE_BUTTON = Buttons(DECREASE_ARROW_iMAGE, "", 50, 250) 
 SECOND_DECREASE_BUTTON = Buttons(DECREASE_ARROW_iMAGE, "", 950, 250)
+# button to start the timer by changing the program page to the custom timer count stage
 START_CUSTOM_BUTTON = Buttons(pygame.transform.scale(pygame.image.load("assets\Button.png"), (450, 50)), "start Custom Timer", SCREENWIDTH/2, SCREENHEIGHT*0.8)
+# creating the custim timer from the custom timer class (which inherits from the timer class)
 CustomTimer = customTimer(0, 0)
+# method to display the aspects of the timer creation screen
 def CustomTimerCreation():
   Screen.fill(WHITE)
+  # displaying page title
   AddText("Create Custom Timer", (200, 50), 60, BLACK)
+  # dip;aying the timer using class method
   CustomTimer.display()
+  # displaying the buttons using the button class method
   MINUTE_INCREASE_BUTTON.draw_button()
   SECOND_INCREASE_BUTTON.draw_button()
   MINUTE_DECREASE_BUTTON.draw_button()
@@ -150,7 +203,7 @@ def CustomTimerCreation():
   START_CUSTOM_BUTTON.draw_button()
 
 
-
+#method to display the counting cutom timer screen
 def CustomTimerCount():
   Screen.fill(WHITE)
   AddText("Study time", (300, 50), 60, BLACK)
@@ -158,7 +211,6 @@ def CustomTimerCount():
   PAUSE_BUTTON.draw_button()
   RESUME_BUTTON.draw_button()
   
-
 
 global Current_Page 
 Current_Page = "Menu"
@@ -170,6 +222,8 @@ while Running:
     # if pygame window x pressed then end the program
     if event.type == pygame.QUIT:
       Running = False
+
+    # checking buttons for being pressed
     if event.type == pygame.MOUSEBUTTONDOWN:
       if (POMODORO_BUTTON.isPressed()) & (Current_Page == "Menu"):
         Current_Page = "Timer"
@@ -195,6 +249,7 @@ while Running:
         Current_Page = "CustomTimerCount"
 
 
+  # claling functions based on the current page being displayed
   match Current_Page:
     case "Menu":
       MenuScreen()
@@ -216,8 +271,11 @@ while Running:
         Current_Page = "Menu"
         CustomTimer.Reset()
 
+    # default case for it other casses not activated
     case _:
       MenuScreen()
 
+  # progressing the game clock in the main loop
   clock.tick(FPS)
+  # updating the game screen
   pygame.display.update()
